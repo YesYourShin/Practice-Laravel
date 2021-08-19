1359549a8e3856a276c71d89043fb3a6280fed85
YesYourShin <ehrl827@naver.com>
2021-08-19 20:49:07 +0900

    commit

--- blog/app/Http/Controllers/RegisterController.php
+++ blog/app/Http/Controllers/RegisterController.php
@@ -7,6 +7,17 @@ use Illuminate\Http\Request;
 class RegisterController extends Controller
 {
     function register (Request $request) {
+        
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $validate = $request->validate([
+            'name' => 'required',
+            'email' => '',
+            'password' => ''
+        ]);
+        
         // dd($request);
         return $request;
     }
