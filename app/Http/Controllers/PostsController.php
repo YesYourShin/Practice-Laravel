2cd53ec42c48b3865808a2ee8d7b013e0db31e4d
YesYourShin <ehrl827@naver.com>
2021-09-13 15:49:01 +0900

    storage images

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -20,7 +20,14 @@ class PostsController extends Controller
             2. 게시글 목록을 만들어주는 blade에 읽어온 데이터를 전달하고 실행
             
         */
-        $posts = Post::all();
+        // select * from posts order by created_at desc
+        // $posts = Post::orderBy('created_at', 'desc')->get();
+
+        // $posts = Post::latest()->get();
+        // $posts = Post::oldest()->get();
+
+        $posts = Post::latest()->paginate(10);
+
         // dd($posts);
         return view('bbs.index', ['posts'=>$posts]);
     }
@@ -48,8 +55,29 @@ class PostsController extends Controller
             'content'=>'required|min:3'
         ]);
 
+        $fileName = null;
+        if($request->hasFile('image')){
+            // dd($request->file('image'));
+            $fileName = time().'_'.
+                $request->file('image')->getClientOriginalName();
+            $path = $request->file('image')
+                ->storeAs('public/images', $fileName);
+                
+            // dd($path);
+        }
+
         // dd($request->all());
-        $input = array_merge($request->all(), ["user_id"=>Auth::user()->id]);
+        $input = array_merge($request->all(), [
+            "user_id"=>Auth::user()->id
+        ]);
+        // 이미지가 있으면 $input에 image 항목 추가
+
+        if($path) {
+            // dd($path.':'.strrpos($path, '/'));
+        
+            $input = array_merge($input, ['image' => $fileName]);
+            // dd($input);
+        }
 
         /*
             $request->all() : ['title'=>'asdf', 'content'=>'asdf']
@@ -62,7 +90,16 @@ class PostsController extends Controller
             input의 내용은 ["title"=>"asdf", 'content"=>"adsf", "user_id"=>1]
         */
 
+        
+        // mass assignment
+        // Eloquent model의 white list인 $fillable에 기술해야 한다.
         Post::create($input);
+
+        // $post = new Post;
+        // $post->title = $input['title'];
+        // $post->content = $input['content'];
+        // ...
+        // $post->save();
 
         // return view('bbs.index', ['posts'=>Post::all()]);
         return redirect()->route('posts.index');
--- resources/views/components/post-list.blade.php
+++ resources/views/components/post-list.blade.php
@@ -19,4 +19,5 @@
           @endforeach
         </tbody>
       </table>
+      {{ $posts->links() }}
 </div>
