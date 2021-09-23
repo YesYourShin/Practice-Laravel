0045887471a32044357e5deb8eb4194a0ebc2992
YesYourShin <ehrl827@naver.com>
2021-09-23 16:53:33 +0900

    2021-09-23 post edit and delete

--- resources/views/bbs/edit.blade.php
+++ resources/views/bbs/edit.blade.php
@@ -2,19 +2,21 @@
     <x-slot name="header">
         <div class="flex justify-between">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('글쓰기 폼') }}
+                {{ __('글수정 폼') }}
             </h2>
-            <button onclick=location.href="{{ route('posts.index') }}" type="button" class="btn btn-info font-bold hover:bg-blue-700 text-white">
-                목록보기
+            <button onclick=location.href="{{ route('posts.show', ['post'=>$post->id]) }}" type="button" class="btn btn-info font-bold hover:bg-blue-700 text-white">
+                상세보기
             </button>
         </div>
     </x-slot>
     <div class="m-4 p-4">
-        <form class="row g-3" action="{{ route('posts.store') }}"method="post" enctype="multipart/form-data">
+        <form class="row g-3" action="{{ route('posts.update', ['post'=>$post->id])}}"method="post" enctype="multipart/form-data">
+            @method('patch')
             @csrf
             <div class="col-12 m-2">
                 <label for="title" class="form-label">제목</label>
-                <input type="text" name="title" class="form-control" id="title" placeholder="제목을 입력하세요." value="{{ old('title') }}">
+                <input type="text" name="title" class="form-control" id="title" 
+                            value="{{ $post->title }}">
                 @error('title')
                 <div class="text-red-800">
                     <span>{{ $message }}</span>
@@ -23,14 +25,20 @@
             </div>
             <div class="col-12">
                 <label for="inputAddress2" class="form-label">글 내용</label>
-                <textarea class="form-control" name="content" id="content" rows="3" >{{ old('content') }}</textarea>
+                <textarea class="form-control" name="content" id="content" rows="3" >{{ $post->content }}</textarea>
                 @error('content')
                 <div class="text-red-800">
                     <span>{{ $message }}</span>
                 </div>
                 @enderror
             </div>
-            <div class="col-12">
+            <div class="col-12 m-2">
+                @if($post->image)
+                    <img class="w-20 h-20 rounded-full" src="{{'/storage/images/'.$post->image}}" 
+                        class="card-img-top" alt="my post image">
+                @else
+                    <span>첨부 이미지 없음</span>
+                @endif
                 <label for="image" class="form-label">첨부 이미지</label>
                 <input type="file" name="image" class="form-control" id="image">
             </div>
