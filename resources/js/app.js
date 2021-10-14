c485b6b4d886924265e219b77061d6fa835a6f23
YesYourShin <ehrl827@naver.com>
2021-10-14 16:53:26 +0900

    comment

--- resources/js/app.js
+++ resources/js/app.js
@@ -29,6 +29,7 @@ window.Swal = require('sweetalert2')
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('like-button', require('./components/LikeButton.vue').default);
+Vue.component('comment-list', require('./components/CommentList.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
