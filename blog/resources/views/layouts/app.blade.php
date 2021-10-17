93d75274628199cbcac10a1bad023412826a968d
YesYourShin <ehrl827@naver.com>
2021-10-18 00:53:34 +0900

    faker data

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -20,5 +20,14 @@
             }
         </style>
     </head>
-    
+    <body class="antialiased">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+            @yield('content')
+            <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+                <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
