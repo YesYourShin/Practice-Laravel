d25c3519b29063b892e9e9f716c30e45507adb11
YesYourShin <ehrl827@naver.com>
2021-11-18 16:57:29 +0900

    20211118

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth')
+            ->except(['show', 'index']);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +20,15 @@ class CarController extends Controller
      */
     public function index()
     {
-        //
+        /*
+            1. DB에서 리스트를 가져온다
+            2. 그 리스트를 블레이드 컴포넌트에게 전달한다.
+        */
+        // $cars = Car::all();
+        // select * from cars order by created_at desc;
+        // $cars = Car::orderBy('created_at', 'desc')->get();
+        $cars = Car::latest()->get();
+        return view('components.cars.index', ['cars'=>$cars]);
     }
 
     /**
---
+++ app/View/Components/cars/Index.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View\Components\cars;
+
+use Illuminate\View\Component;
+
+class Index extends Component
+{
+
+    public $cars;
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($cars)
+    {
+        $this->cars = $cars;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.cars.index');
+    }
+}
